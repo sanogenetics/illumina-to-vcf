c1731796c539acd62cf23869e111342eaf0506a3
Adam Faulconbridge <adam@sanogenetics.com>
2022-06-21 15:29:36 +0100

    refactor blocklist from PR #2

--- illumina2vcf/__init__.py
+++ illumina2vcf/__init__.py
@@ -9,22 +9,25 @@ logger = logging.getLogger(__name__)
 class Converter:
     reference_filename: str
     reference_index_filename: str
+    blocklist_filename: str
     delimiter: str
 
     def __init__(
         self,
         reference_filename: str,
         reference_index_filename: str,
+        blocklist_filename: str = "",
         delimiter: str = ",",
         buildname="GRCh38",
     ) -> None:
         self.reference_filename = reference_filename
         self.reference_index_filename = reference_index_filename
+        self.blocklist_filename = blocklist_filename
         self.delimiter = delimiter
         self.buildname = buildname
 
     def convert(self, source, destination) -> None:
-        reader = IlluminaReader(self.delimiter)
+        reader = IlluminaReader(self.delimiter, self.blocklist_filename)
         vcfgenerator = VCFMaker(self.reference_filename, self.reference_index_filename)
         # read source header
         date, header_source = reader.parse_header(source)
--- illumina2vcf/__main__.py
+++ illumina2vcf/__main__.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
 
     logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
     parser = argparse.ArgumentParser()
+    # TODO make non-optional argument
     parser.add_argument("--fasta", help="path to reference fasta, and .fai index")
     parser.add_argument(
         "--tab", "-t", action="store_const", const="\t", dest="delim", help="tab as delimitor in source"
@@ -17,9 +18,12 @@ if __name__ == "__main__":
     parser.add_argument(
         "--comma", "-c", action="store_const", const=",", dest="delim", help="comma as delimitor in source"
     )
+    parser.add_argument(
+        "--blocklist", default="ref/GSAMD-24v3-0-EA_20034606_A2_blocklist.txt", help="path to probe name blocklist"
+    )
     args = parser.parse_args()
 
-    converter = Converter(args.fasta, args.fasta + ".fai", args.delim)
+    converter = Converter(args.fasta, args.fasta + ".fai", args.blocklist, args.delim)
 
     # read from stdin as uncompressed text
     # write to stdout as uncompressed vcf
