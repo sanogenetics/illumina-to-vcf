8115911b76ca52d2679e4bd2017a4e215cfbd709
Adam Faulconbridge <adam@sanogenetics.com>
2022-07-04 15:29:59 +0000

    fix extra args

--- docker/run.sh
+++ docker/run.sh
@@ -2,4 +2,4 @@
 set -e
 set -u
 
-aws s3 cp $1 - | bgzip -d | python3.9 -m illumina2vcf $3 | bgzip | aws s3 cp - $2
+aws s3 cp $1 - | bgzip -d | python3.9 -m illumina2vcf ${@:3} | bgzip | aws s3 cp - $2
