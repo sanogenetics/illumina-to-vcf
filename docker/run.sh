2bb972aa906fc83cd976f4edd5109890ff4f04c0
Adam Faulconbridge <adam@sanogenetics.com>
2022-09-01 11:18:53 +0000

    add comment

--- docker/run.sh
+++ docker/run.sh
@@ -7,6 +7,11 @@ set -u
 # arg 2 is the output s3 url
 # args other are passed to illumina2vcf
 
+# AWS_CLI_ARGS environment variable can be set to e.g. --endpoint-url=http://moto:5000
+# tabix has its own variables e.g. HTS_S3_HOST=moto:5000 and HTS_S3_ADDRESS_STYLE=path
+# may also need to set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY even if IAM not implemented
+# may also need to enforce HTTP not HTTPS in scheme e.g. s3+http://....
+
 # first have to sort the input rows
 # by sample, chromosome, then position _numerically_ i.e. 1,2,10
 # do conversion, then sort again (not needed?)
