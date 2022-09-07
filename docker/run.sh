251b90b68a2f929a9f1bc963dfeade7adf620a85
Adam Faulconbridge <adam@sanogenetics.com>
2022-09-07 09:57:29 +0100

    readd sort

--- docker/run.sh
+++ docker/run.sh
@@ -22,6 +22,7 @@ set -u
 # sort by chromosome, position, probe num, samplenum
 { aws ${AWS_CLI_ARGS} s3 cp $1 - | funzip | head; \
   aws ${AWS_CLI_ARGS} s3 cp $1 - | funzip | tail -n+11 \
+  | sort -V -k9,10 -k4,5
 } | python3.9 -m illumina2vcf ${@:3} | bcftools sort -Oz | aws ${AWS_CLI_ARGS} s3 cp - $2
 
 s3role tabix $2
