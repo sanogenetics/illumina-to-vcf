b712282db31c2d3bd73c9a705c01f782cb4884a2
Adam Faulconbridge <adam@sanogenetics.com>
2022-09-09 11:21:56 +0000

    tag in ci not build

--- docker/build.sh
+++ docker/build.sh
@@ -5,20 +5,17 @@ set -x
 # build the slim default version
 docker build \
   -t illumina2vcf:latest \
-  -t sanogenetics/illumina2vcf:latest \
   -f docker/Dockerfile \
   .
 
 # build the reference version (~3GB bigger)
 docker build \
   -t illumina2vcf:latest-ref \
-  -t sanogenetics/illumina2vcf:latest-ref \
   -f docker/Dockerfile.ref \
   .
 
 # build the test reference version (~5MB bigger)
 docker build \
   -t illumina2vcf:latest-test-ref \
-  -t sanogenetics/illumina2vcf:latest-test-ref \
   -f docker/Dockerfile.test-ref \
   .
